3f32342571e13af2706670b6ad063467a5f31f97
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-06-23 13:30:11 -0500

    agregando arregglos de objetos

--- assets/js/propiedad_venta.js
+++ assets/js/propiedad_venta.js
@@ -1,13 +1,45 @@
 const propiedades_venta = [
-    {
-    nombre: '',
-    src: '',
-    descripcion: '',
-    ubicacion: '',
-    habitaciones: 0,
-    costo: 0,
+  {
+    nombre: "Exclusive Surco",
+    src: "",
+    descripcion: "Lanzamiento Looo And Feel 2, un proyecto arquitectonico audaz e imponente",
+    ubicacion: "Av. Encalada 1344. Surco",
+    habitaciones: 3,
+    costo: 319919,
     smoke: false,
-    pets: true
-    }
-    ]
-    
+    pets: true,
+  },
+
+
+  {
+    nombre: "Salaverry 571",
+    src: "",
+    descripcion: "Lanzamiento Looo And Feel 2, un proyecto arquitectonico audaz e imponente",
+    ubicacion: "Avenida General Felipe Salaverry 571",
+    habitaciones: 2,
+    costo: 366000,
+    smoke: true,
+    pets: false,
+  },
+  {
+    nombre: "Aster Basadre",
+    src: "",
+    descripcion: "Lanzamiento Looo And Feel 2, un proyecto arquitectonico audaz e imponente",
+    ubicacion: "Avenida Cesar Vallejo 320",
+    habitaciones: 3,
+    costo: 219600,
+    smoke: false,
+    pets: true,
+  },
+
+  {
+    nombre: "Benavides Deluxe",
+    src: "",
+    descripcion: "Lanzamiento Looo And Feel 2, un proyecto arquitectonico audaz e imponente",
+    ubicacion: "Av. 28 de julio 1365 miraflores",
+    habitaciones: 4,
+    costo: 750000,
+    smoke: true,
+    pets: false,
+  },
+];
